6c3a13574700700e4fe4ffee60ecb10ae51c614c
jagLas <jglasher@gmail.com>
2023-05-25 15:07:25 -0400

    improved solution

--- Operators practice/largest_perimeter_triangle.py
+++ Operators practice/largest_perimeter_triangle.py
@@ -1,26 +1,26 @@
 def largestPerimeter(nums):
-    nums = sorted(nums, reverse=True)
+    nums = sorted(nums)
 
     while len(nums) >= 3:
-        s1 = nums[0]
-        s2 = nums[1]
-        s3 = nums[2]
+        s1 = nums[-1]
+        s2 = nums[-2]
+        s3 = nums[-3]
 
         if s3 + s2 > s1:
             return s1 + s2 + s3
         
-        nums.pop(0)
+        nums.pop()
     
     return 0
 
 
 
 
-# nums = [2,1,2]
-# print(largestPerimeter(nums))
+nums = [2,1,2]
+print(largestPerimeter(nums))
 
-# nums = [1,2,1,10]
-# print(largestPerimeter(nums))
+nums = [1,2,1,10]
+print(largestPerimeter(nums))
 
 nums = [3,6,2,3]
 print(largestPerimeter(nums))
