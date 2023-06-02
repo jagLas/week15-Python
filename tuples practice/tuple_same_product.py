2d42bc23d7e481137bfbd2bd64da0cc0b70d2adf
jagLas <jglasher@gmail.com>
2023-06-02 10:44:01 -0400

    better solution

--- tuples practice/tuple_same_product.py
+++ tuples practice/tuple_same_product.py
@@ -1,27 +1,24 @@
+import math
+
 def tupleSameProduct(nums):
-    # creates all unique pairs numbers and stores their products
+    # creates all unique products and store the pair that makes them
     products = {}
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
-            pair = nums[i] , nums[j]
-            products[pair] = nums[i]*nums[j]
-    print(products)
+            a = nums[i]
+            b = nums[j]
+            product = a * b
+            try:
+                products[product].append((a, b))
+            except:
+                products[product] = [(a, b)]
 
-    # find pairs whose products match
     count = 0
-    for pair in products:
-        for pair2 in products:
-            if pair == pair2:
-                continue
-            if pair[0] in pair2:
-                continue
-            if pair[1] in pair2:
-                continue
-            if pair[0] * pair[1] == pair2[0] * pair2[1]:
-                count += 1
-
-
-    # adjust count based on rearranging pairs (*4 maybe?)
+    for product in products:
+        length = len(products[product])
+        if length > 1:
+            combinations = math.factorial(length) / math.factorial(length - 2) 
+            count += int(combinations)
 
     return count * 4
 
@@ -32,3 +29,7 @@ print(tupleSameProduct(case1))
 print('\ncase 2')
 case2 = [1,2,4,5,10]
 print(tupleSameProduct(case2))
+
+print('\ncase 3')
+case3 = [2,3,4,6,8,12]
+print(tupleSameProduct(case3))
