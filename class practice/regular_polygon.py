2a9e1dc9a7b29e063a67c988bec327afc7d61920
jagLas <jglasher@gmail.com>
2023-06-09 18:24:44 -0400

    more practice

--- class practice/regular_polygon.py
+++ class practice/regular_polygon.py
@@ -14,20 +14,41 @@ class RegularPolygon():
     def __init__(self, sides = 3, length = 1) -> None:
         if sides < 3:
             raise Exception('A polygon must have at least 3 sides')
-        self._s = sides
-        self._l = length
+        self.num_sides = sides
+        self.length = length
         self.type = 'Polygon'
 
     def identify_polygon(self):
-        pass
+        sides = self.num_sides
+        if (sides == 3):
+            self.type = 'Triangle'
+        elif sides == 4:
+            self.type = 'Quadrilateral'
+        elif sides == 5:
+            self.type = 'Pentagon'
+        elif sides == 6:
+            self.type = 'Hexagon'
+        elif sides == 7:
+            self.type = 'Heptagon'
+        elif sides == 8:
+            self.type = 'Octagon'
+        elif sides == 9:
+            self.type = 'Nonagon'
+        elif sides == 10:
+            self.type = 'Decagon'
+        else:
+            self.type = f'Polygon with {sides} sides'
 
     @classmethod
-    def polygon_factory(cls):
-        pass
+    def polygon_factory(cls, lst):
+        return [cls(sides, length) for sides, length in lst]
 
     @staticmethod
-    def get_permiter(polygon):
-        pass
+    def get_perimeter(polygon):
+        return polygon.num_sides * polygon.length
+    
+    def __repr__(self) -> str:
+        return f'{self.num_sides} sided polygon of length {self.length}'
 
 
 pentagon = RegularPolygon(5, 5)
