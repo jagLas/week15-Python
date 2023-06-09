81c66c63f123b2e0229a4691f9f101f048341932
jagLas <jglasher@gmail.com>
2023-06-09 18:50:21 -0400

    more practice

--- class practice/getters_and_setters.py
+++ class practice/getters_and_setters.py
@@ -1,4 +1,15 @@
 # Write your class here.
+class Game:
+    def __init__(self) -> None:
+        self._score = 0
+    
+    @property
+    def score(self):
+        return self._score
+    
+    @score.setter
+    def score(self, value):
+        self._score += value * 10
 
 my_game = Game()
 print(my_game.score) # 0
