5aa158089cb22d6bec785b27b227b143df011f8a
jagLas <jglasher@gmail.com>
2023-07-07 08:53:48 -0400

    unit tests through 8

---
+++ practice-for-week-17-tdd-with-python-long-practice/app/roman_numerals.py
@@ -0,0 +1,17 @@
+def parse(numeral):
+    if numeral == 'I':
+        return 1
+    elif numeral == 'II':
+        return 2
+    elif numeral == 'III':
+        return 3
+    elif numeral == 'IV':
+        return 4
+    elif numeral == 'V':
+        return 5
+    elif numeral == 'VI':
+        return 6
+    elif numeral == 'VII':
+        return 7
+    elif numeral == 'VIII':
+        return 8
